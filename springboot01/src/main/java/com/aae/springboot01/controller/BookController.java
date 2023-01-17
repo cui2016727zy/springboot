ee5b4ed53d0eb4f9ae48320315ada480e2d608c7
Aae <1289301299@qq.com>
2023-01-17 22:24:40 +0800

    second commit

--- springboot01/src/main/java/com/aae/springboot01/controller/BookController.java
+++ springboot01/src/main/java/com/aae/springboot01/controller/BookController.java
@@ -1,20 +1,48 @@
 package com.aae.springboot01.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aae.springboot01.Util.R;
+import com.aae.springboot01.dao.BookDao;
+import com.aae.springboot01.pojo.Book;
+import com.aae.springboot01.service.IBookService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    @Autowired
+    private BookDao bookDao;
 
-
+    @Autowired
+    private IBookService bookService;
 
     @GetMapping
-    public String getById(){
-        System.out.println("springboot");
-        return "springboot";
+    public R getAll(){
+        return new R(true,bookService.list());
+    }
+    @PostMapping
+    public R save(@RequestBody Book book){
+        return new R(bookService.save(book));
+    }
+    @PutMapping
+    public R update(@RequestBody Book book){
+        return new R(bookService.modify(book));
+    }
+    @DeleteMapping("{id}")
+    public R delete(@PathVariable Integer id){
+        return new R(bookService.delete(id));
+    }
+    @GetMapping("{id}")
+    public R getById(@PathVariable Integer id){
+        return new R(true,bookService.getById(id));
+    }
+    @GetMapping("{currentPage}/{pageSize}")
+    public R getPage(@PathVariable int currentPage,@PathVariable int pageSize){
+        return new R(true,bookService.getPage(currentPage, pageSize));
     }
 }
--- springboot01/src/test/java/com/aae/springboot01/Springboot01ApplicationTests.java
+++ springboot01/src/test/java/com/aae/springboot01/Springboot01ApplicationTests.java
@@ -1,6 +1,12 @@
 package com.aae.springboot01;
 
 import com.aae.springboot01.dao.BookDao;
+import com.aae.springboot01.pojo.Book;
+import com.aae.springboot01.service.IBookService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +17,45 @@ class Springboot01ApplicationTests {
     @Autowired
     private BookDao bookDao;
 
+    @Autowired
+    private IBookService bookService;
+
     @Test
     void contextLoads() {
-        System.out.println(bookDao.getById(1));
+        System.out.println(bookDao.selectById(1));
+    }
+    @Test
+    void test(){
+        Book book = new Book();
+        book.setType("测试数据");
+        book.setName("测试数据");
+        book.setDescription("123");
+        bookService.save(book);
     }
 
+    @Test
+    void testPage(){
+        IPage page=new Page(2,5);
+        bookDao.selectPage(page,null);
+    }
 
+    @Test
+    void testGetBy(){
+//        QueryWrapper<Book> queryWrapper=new QueryWrapper();
+//        queryWrapper.like("name","java");
+//        bookDao.selectList(queryWrapper);
+        LambdaQueryWrapper<Book> lambdaQueryWrapper=new LambdaQueryWrapper();
+        lambdaQueryWrapper.like(Book::getName,"java");
+        bookDao.selectList(lambdaQueryWrapper);
+    }
+
+    @Test
+    void testupdate(){
+        Book book=new Book();
+        book.setId(19);
+        book.setName("sada");
+        book.setType("123");
+        book.setDescription("456");
+        bookService.update(book,null);
+    }
 }
